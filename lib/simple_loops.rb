74f3da91aa66200498e85d20840716ecaddf4ed3
David Jun <djun301@gmail.com>
2021-01-02 03:22:36 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,10 +14,10 @@ def loop_message_n_times(message, limit)
   end
 end
 
-def return_string_array(array)
+def output_array(array)
   count = 0
   while count < array.length do
- 
+    puts array[count]
     count += 1
   end
 end
